ef089110fd51f7bdb959ca7c36c8557bba2588c8
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-05-01 11:42:32 -0600

    Initial commit

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,14 @@
+alert ("Hello World!");
+window.alert("Here\'s an new Window");
+var A = "How to write and display a string value.";
+document.write(A);
+document.write("\"Why can\'t this display the quote\?\""
++ "Nevermind\, it displayed the quotes\"");
+var B = ("iPods" + " are really popular.")
+document.write(B)
+var Instructors  = "Instructors", Instructor1 = "Holden", Instructor2 = "Jen"
+document.write(Instructor1);
+var Instructors = Instructors.fontcolor("orange");
+document.write(Instructors);
+document.write(4+5);
+
