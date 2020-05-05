b5a364fab575be2b5ab8fb96b7772379606ce43d
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-05-05 10:56:08 -0600

    pg 91 submission

---
+++ JS/Basic_JavaScript_4.js
@@ -0,0 +1,11 @@
+function my_dictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
