742f558ebf9a529c08903759f581ae77afa673b2
TDBoren <63603319+TDBoren@users.noreply.github.com>
2020-05-05 05:41:35 -0600

    pg 63 resubmit

--- JS/Basic_JavaScript_2.js
+++ JS/Basic_JavaScript_2.js
@@ -4,11 +4,15 @@ function myFunction() {
     var currentVal = x.value;
     
     if (defaultVal == currentVal) {
-      document.getElementById("demo").innerHTML = "Default value and current value is the same: "
+      document.getElementById("demo_1").innerHTML = "Default value and current value is the same: "
       + x.defaultValue + " and " + x.value
       + "<br>Change the number of number field to see the difference!";
     } else {
-      document.getElementById("demo").innerHTML = "The default value was: " + defaultVal
+      document.getElementById("demo_1").innerHTML = "The default value was: " + defaultVal
       + "<br>The new, current value is: " + currentVal;
     }
+    var x = 5;
+    var y = 2;
+    var z = x + y;
+    document.getElementById("demo_2").innerHTML = z;
 }
